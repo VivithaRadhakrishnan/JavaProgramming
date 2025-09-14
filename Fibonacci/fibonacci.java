197f1c7f4d5a25fbaf2ff2db8637459e1ba59b79
VivithaRadhakrishnan <vivithatesting03@gmail.com>
2025-09-14 13:43:40 +0530

    Create file for fibonacciSeries

---
+++ Fibonacci/fibonacci.java
@@ -0,0 +1,22 @@
+package Fibonacci;
+import java.util.Scanner;
+
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+
+public class fibonacci {
+    public static void main(String[] args) {
+        int first = 0;
+        int second = 1;
+                
+        System.out.println("Enter the count of digits to be generated");
+        Scanner input = new Scanner(System.in);
+        int count = input.nextInt();
+        System.out.print("The number generated: "+first +" "+ second);
+        for (int i=2;i<count;i++ ){
+        int temp = first+second;
+        first = second;
+        second = temp;
+        System.out.print(" " +temp);
+    }
+    }
+}
